1a5798ef7e7b93a91ae0f9ae0b2551b1406e2d49
AbdullahJawed1 <82279451+AbdullahJawed1@users.noreply.github.com>
2024-05-26 23:42:21 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 
-const supabase = createClient("https://jurxnvcaqgooqpqgzkdi.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1cnhudmNhcWdvb3FwcWd6a2RpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNjcwOTAyMSwiZXhwIjoyMDMyMjg1MDIxfQ.UaK5EngwB0iZwefXsN5JYYEoY0asWXAI3SeTrcQ96OY");
+const supabase = createClient("DB url here", "secret key");
 
 const app = express();
 app.use(cors());
